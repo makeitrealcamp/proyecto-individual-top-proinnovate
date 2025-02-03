d1a6ee556300f08795d751b45ab9245fa3c9cca8
anayib <nayib.abdala@gmail.com>
2025-02-03 14:27:12 +0100

    Add proyect description

---
+++ README.mk
@@ -0,0 +1,54 @@
+# PROYECTO INDIVIDUAL PROINNOVATE - TOP FULL STACK DEVELOPER
+
+## Descripción del Proyecto
+El objetivo de este reto es desarrollar una aplicación completa que integre funcionalidades avanzadas de backend y frontend, cumpliendo con los siguientes requisitos técnicos. 
+Este proyecto te permitirá demostrar tu capacidad para trabajar de forma autónoma, implementar tecnología moderna y crear un producto funcional y desplegado.
+
+## Requisitos del Proyecto
+- Registro y autenticación de usuarios.
+- Almacenar información en una base de datos (Postgresql).
+- El backend debe estar implementado en Node.js (Express) y el frontend en React. Usar TypeScript es opcional. 
+- Debe incluir carga de imágenes y envío de correos.
+- Debe tener al menos una de las siguientes funcionalidades:
+  - Pagos
+  - Presentar información en un mapa.
+  - Tiempo Real (Web Sockets).
+- Debe estar desplegada en Internet (Netllify, Heroku, Vercel)
+- Debe tener pruebas end-to-end con Cypress.
+- Se debe presentar un video a modo de DEMO de no mas de 7 minutos donde el estudiantes exponga los siguientes temas:
+	- Introducción (Nombre del proyecto, URL’s).
+	- Descripción de la aplicación.
+	-	Metodología (Jira, Github, SCRUM, sprints)
+	- Tecnologías (React, Material, Express, Socket.io, MongoDB, Postgresql, etc)
+	- Diagrama de como esta distribuida su aplicacion (arquitectura) backend, frontend, servicios extras
+Como respuesta de esta mision deberas agregar los links de:
+- El enlace a github (en caso de tener varios repos incluirlos todos)
+- El video de maximo 7 minutos explicando su proyecto
+- El enlace al despliegue en producción
+
+## Fecha maxima de entrega preliminar
+
+Lunes 17 de febero de 2025 a las 11:00 pm. Después de esta hora no se recibirán los proyectos. 
+
+Una vez entregado el proyecto tu mentor de apoyo te dará retroalimentación y de necesitar correcciones tendrás hasta el 25 de febrero para presentarlas. 
+
+## Fecha máxima de entrega final
+
+Martes 25 de febrero de 2025. 
+
+## Grabaciones de clases
+
+https://github.com/makeitrealcamp/top-fs-dev-proinnovate-i-2024/blob/main/clases.md 
+
+## Herramientas de AI
+
+- https://chatgpt.com/
+- https://chat.deepseek.com/ 
+- https://v0.dev 
+- https://claude.ai/ 
+
+## Metor asignado para tu evaluación
+
+- Nayruth Calla
+- Slack: @Nay Calla
+- Email: nayruth.calla@gmail.com 
